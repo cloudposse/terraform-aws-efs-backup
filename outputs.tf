586c9a04018d3573139f80e2beab7c2f9c637f23
Sergey Vasilyev <s2504s@gmail.com>
2017-08-03 15:37:02 +0300

    Added LICENSE .gitignore outputs.tf

---
+++ outputs.tf
@@ -0,0 +1,25 @@
+output "aws_vpc" {
+  value = "${data.aws_vpc.vpc.id}"
+}
+
+output "aws_subnet_ids" {
+  value = "${data.aws_subnet_ids.all.ids}"
+}
+
+output "aws_s3_bucket S3" {
+  value = "${aws_s3_bucket.s3.bucket_domain_name}"
+}
+
+output "aws_s3_bucket efs_backups" {
+  value = "${aws_s3_bucket.efs_backups.bucket_domain_name}"
+}
+
+//output "efs_dns_names" {
+//  value = "${join(", ", aws_efs_mount_target.efs.*.dns_name)}"
+//}
+
+
+//output "datapipeline_ids" {
+//  value = "${aws_cloudformation_stack.datapipeline.*.outputs["DataPipelineId"]}"
+//}
+
