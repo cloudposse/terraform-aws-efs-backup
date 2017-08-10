bb2daad6609c0250e03df081e038ea71e90a5a40
Sergey Vasilyev <s2504s@gmail.com>
2017-08-10 15:56:08 +0300

    Changed outputs

--- outputs.tf
+++ outputs.tf
@@ -1,17 +1,9 @@
-output "vpc_id" {
-  value = "${data.aws_vpc.default.id}"
-}
-
-output "bucket_name" {
+output "logs_bucket_name" {
   value = "${aws_s3_bucket.logs.bucket_domain_name}"
 }
 
-output "backups_domain_name" {
+output "backups_bucket_name" {
   value = "${aws_s3_bucket.backups.bucket_domain_name}"
-}
-
-output "efs_backups_id" {
-  value = "${aws_s3_bucket.backups.id}"
 }
 
 output "datapipeline_ids" {
