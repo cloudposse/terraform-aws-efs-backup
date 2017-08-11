563feeccb26bc9eadafdbda4ebfb8afd133b178d
Sergey Vasilyev <s2504s@gmail.com>
2017-08-11 19:53:55 +0300

    Fixed names

--- iam.tf
+++ iam.tf
@@ -11,13 +11,27 @@ data "aws_iam_policy_document" "ec2" {
   }
 }
 
+module "ec2_label" {
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=0.1.0"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "ec2"
+}
+
 resource "aws_iam_role" "ec2" {
-  name               = "${module.tf_label.id}-ec2"
+  name               = "${module.ec2_label.id}"
   assume_role_policy = "${data.aws_iam_policy_document.ec2.json}"
 }
 
+module "instance_label" {
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=0.1.0"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "instance"
+}
+
 resource "aws_iam_instance_profile" "instance_profile" {
-  name = "${module.tf_label.id}-instance"
+  name = "${module.instance_label.id}"
   role = "${aws_iam_role.ec2.name}"
 }
 
@@ -34,19 +48,26 @@ data "aws_iam_policy_document" "datapipeline_resource" {
   }
 }
 
+module "resource_label" {
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=0.1.0"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "resource"
+}
+
 resource "aws_iam_role" "datapipeline_resource" {
-  name               = "${module.tf_label.id}-resource"
+  name               = "${module.resource_label.id}"
   assume_role_policy = "${data.aws_iam_policy_document.datapipeline_resource.json}"
 }
 
 resource "aws_iam_policy_attachment" "datapipeline_resource" {
-  name       = "${module.tf_label.id}-resource"
+  name       = "${module.resource_label.id}"
   roles      = ["${aws_iam_role.datapipeline_resource.name}"]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2RoleforDataPipelineRole"
 }
 
 resource "aws_iam_instance_profile" "datapipeline_resource" {
-  name = "${module.tf_label.id}-resource"
+  name = "${module.resource_label.id}"
   role = "${aws_iam_role.datapipeline_resource.name}"
 }
 
@@ -67,13 +88,20 @@ data "aws_iam_policy_document" "datapipeline_role" {
   }
 }
 
+module "role_label" {
+  source    = "git::https://github.com/cloudposse/tf_label.git?ref=0.1.0"
+  namespace = "${var.namespace}"
+  stage     = "${var.stage}"
+  name      = "role"
+}
+
 resource "aws_iam_role" "datapipeline_role" {
-  name               = "${module.tf_label.id}-role"
+  name               = "${module.role_label.id}"
   assume_role_policy = "${data.aws_iam_policy_document.datapipeline_role.json}"
 }
 
 resource "aws_iam_policy_attachment" "datapipeline_role" {
-  name       = "${module.tf_label.id}-role"
+  name       = "${module.role_label.id}"
   roles      = ["${aws_iam_role.datapipeline_role.name}"]
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSDataPipelineRole"
 }
