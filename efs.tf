0af2dd72010de218e63c371230c365828cc08b51
Sergey Vasilyev <s2504s@gmail.com>
2017-08-11 19:56:35 +0300

    Changed efs.tf

--- efs.tf
+++ efs.tf
@@ -1,5 +1,5 @@
 # Get list of EFS ids
 data "aws_efs_file_system" "default" {
-  count          = "${length(efs_ids)}"
+  count          = "${length(var.efs_ids)}"
   file_system_id = "${element(var.efs_ids, count.index)}"
 }
