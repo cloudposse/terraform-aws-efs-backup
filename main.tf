e5e377babce46dd431b5d454ed1d288e4ea4d068
Erik Osterman <erik@cloudposse.com>
2017-09-22 12:11:38 -0700

    Rename Repo (#7)

--- main.tf
+++ main.tf
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 module "label" {
-  source    = "git::https://github.com/cloudposse/tf_label.git?ref=0.1.0"
+  source    = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
   namespace = "${var.namespace}"
   stage     = "${var.stage}"
   name      = "${var.name}"
