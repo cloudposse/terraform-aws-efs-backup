f1cd241fe973ca2172f2b4582385db5929b96473
aknysh <aknysh>
2018-01-08 00:12:26 -0500

    Bump `terraform-null-label` version to `0.3.1`

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 module "logs_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
@@ -15,7 +15,7 @@ resource "aws_s3_bucket" "logs" {
 }
 
 module "backups_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.2.1"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
