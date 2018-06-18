97081667ca736d780259337cfeb2fd25281d78af
Aleksey Motonakha <alm@itsvit.org>
2018-06-18 09:26:19 +0300

    update label, fix output

--- cloudformation.tf
+++ cloudformation.tf
@@ -1,5 +1,5 @@
 module "sns_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.5"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
@@ -22,7 +22,7 @@ resource "aws_cloudformation_stack" "sns" {
 }
 
 module "datapipeline_label" {
-  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.1"
+  source     = "git::https://github.com/cloudposse/terraform-null-label.git?ref=tags/0.3.5"
   namespace  = "${var.namespace}"
   stage      = "${var.stage}"
   name       = "${var.name}"
--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,15 @@
 output "logs_bucket_name" {
-  value = "${aws_s3_bucket.logs.bucket_domain_name}"
+  value = "${join("", aws_s3_bucket.logs.bucket_domain_name)}"
 }
 
 output "backups_bucket_name" {
-  value = "${aws_s3_bucket.backups.bucket_domain_name}"
+  value = "${join("", aws_s3_bucket.backups.bucket_domain_name)}"
 }
 
 output "datapipeline_ids" {
-  value = "${aws_cloudformation_stack.datapipeline.*.outputs["DataPipelineId"]}"
+  value = "${join("", list(aws_cloudformation_stack.datapipeline.*.outputs["DataPipelineId"]))}"
 }
 
 output "security_group_id" {
-  value = "${aws_security_group.datapipeline.id}"
+  value = "${join("", aws_security_group.datapipeline.id)}"
 }
