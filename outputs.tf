8e75969983ce57a9a2e7c636f514631fa049ab88
Vadim Hleif <vadzim.hleif@gmail.com>
2018-07-17 11:49:41 +0300

    Migrate readme yaml (#26)
    * Migrate readme yaml

--- outputs.tf
+++ outputs.tf
@@ -1,15 +1,19 @@
 output "logs_bucket_name" {
-  value = "${aws_s3_bucket.logs.bucket_domain_name}"
+  value       = "${aws_s3_bucket.logs.bucket_domain_name}"
+  description = "Logs bucket name"
 }
 
 output "backups_bucket_name" {
-  value = "${aws_s3_bucket.backups.bucket_domain_name}"
+  value       = "${aws_s3_bucket.backups.bucket_domain_name}"
+  description = "Backups bucket name"
 }
 
 output "datapipeline_ids" {
-  value = "${aws_cloudformation_stack.datapipeline.outputs["DataPipelineId"]}"
+  value       = "${aws_cloudformation_stack.datapipeline.outputs["DataPipelineId"]}"
+  description = "Datapipeline ids"
 }
 
 output "security_group_id" {
-  value = "${aws_security_group.datapipeline.id}"
+  value       = "${aws_security_group.datapipeline.id}"
+  description = "Security group id"
 }
